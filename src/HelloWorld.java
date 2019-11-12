21cbf4a053156a45efbcb99f0f5c8be5115824f3
Wenjing <he.wenjing99@gmail.com>
2019-11-12 14:25:25 -0700

    print 10

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		IntStream.rangeClosed(1, 5)
+		IntStream.rangeClosed(1, 10)
 			.forEach(i -> System.out.println(i));
 	}
 
